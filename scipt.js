0c8e34a2bb4c1e481bf22d5891a7d04a826babba
3ug3ear <116048121+3ug3ear@users.noreply.github.com>
2022-11-02 19:19:59 -0700

    completed activity one

---
+++ scipt.js
@@ -0,0 +1,7 @@
+let myGreeting = "Why Hello There";
+console.log(myGreeting);
+const pi = 3.14;
+console.log(pi);
+let myNumber = 1193;
+console.log(myNumber);
+console.log(Math.min( myNumber, pi));
